9aa8ecb45ab0ed5a763c32e7ae57858954059fb1
anas-qureshi-30 <anasqureshi6556@gmail.com>
2025-12-22 18:53:53 +0530

    Fix Vercel deployment

--- app.py
+++ app.py
@@ -142,5 +142,5 @@ def aiGenQuestion():
     result=aiQuestion.aiQuestionGenerator(jobDescription)
     return jsonify({"result":result})
 
-if __name__=='__main__':
-    app.run(debug=True)
+# if __name__=='__main__':
+#     app.run(debug=True)
